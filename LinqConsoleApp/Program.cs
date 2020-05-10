6f8f2c2437dc360096fe606521a8657c48c393a8
Garik Hayrapetyan <garhay01@gmail.com>
2020-05-10 14:41:29 +0200

    Add project files.

---
+++ LinqConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LinqConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var d = new LinqSamples();
+            //d.Task1();
+            d.Task3();
+            d.Task4();
+        }
+    }
+}
